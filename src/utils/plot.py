513b22fce26e25115bd59bfbbe1beae930dca8d1
bkubick <bjk2158@columbia.edu>
2023-07-29 08:12:03 -0500

    Functionize the classification report plot

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -8,7 +8,7 @@ from typing import List, Optional, Tuple, Union
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 import tensorflow as tf
 
 Array = Union[tf.Tensor, np.ndarray]
@@ -162,6 +162,46 @@ def plot_confusion_matrix(y_true: Array,
                     size=cell_text_size)
     
     fig.show()
+
+
+def plot_classification_f1_report(y_labels: List[int], y_pred: List[int], class_names: List[str]):
+    """ Plots the classification report.
+    
+        Args:
+            y_labels (List[int]): The true labels.
+            y_pred (List[int]): The predicted labels.
+            class_names (List[str]): The class names.
+    """
+    model_classification_report = classification_report(y_labels, y_pred, output_dict=True)
+
+    # Get the f1 score metric and the corresponding class name
+    class_name_to_f1_score = {}
+    for class_number, metrics in model_classification_report.items():
+        # Multiple non-numeric keys occur which we don't want to store
+        try:
+            class_number = int(class_number)
+        except:
+            continue
+
+        class_name=class_names[class_number]
+        class_name_to_f1_score[class_name] = metrics['f1-score']
+
+    # Turn to a dataframe
+    class_name_to_f1_score_df = pd.DataFrame({
+        'class_name': class_name_to_f1_score.keys(),
+        'f1_score': class_name_to_f1_score.values()
+    })
+
+    # Sort the dataframe
+    class_name_to_f1_score_df = class_name_to_f1_score_df.sort_values('f1_score', ascending=True)
+
+    # Plotting the data
+    fig, ax = plt.subplots(figsize=(12,25))
+    scores = ax.barh(range(len(class_name_to_f1_score_df)), class_name_to_f1_score_df['f1_score'].values)
+    ax.set_yticks(range(len(class_name_to_f1_score_df)))
+    ax.set_yticklabels(class_name_to_f1_score_df['class_name'])
+    ax.set_xlabel('F1 Score')
+    ax.set_title('F1 Score of Predictions for each Class')
 
 
 # ============================== IMAGE PLOTTING ==============================
